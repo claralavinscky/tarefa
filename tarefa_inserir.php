1e6725d5af16ad827f97b9f1a9e058e40f90cac9
claralavinscky <claralaavinscky@gmail.com>
2025-10-06 10:46:22 -0300

    terminei best

--- tarefa_inserir.php
+++ tarefa_inserir.php
@@ -9,6 +9,34 @@
    ?>
 </head>
 <body>
-    <h1>Tarefa inserida</h1> 
+
+    <?php 
+    // operação de insert
+    // 1° passo: capturar cada registro que deve ser inserido na tabela
+    // insert into() values()
+        
+    $descricao = $_POST["txtDescricao"];
+    $data_entrega = $_POST["txtData"];
+    $prioridade = $_POST["txtPrioridade"];
+    $responsavel = $_POST["txtResponsavel"];
+    
+    //2° preparar o comando SQL que será executado
+    // criamos uma variavél e passamos os parametros como (?)
+    // cada parametro ficará com interrogação (?)
+    $sql = "INSERT INTO tarefa(descricao,data_entrega,prioridade,responsavel) VALUES(?,?,?,?)" ;
+    //3°VINCULAR ONDE O COMANDO SQL SERA EXECUTADO 
+    $comando = $conexao->prepare($sql);
+    //4° associar cada(?) com seus valores respectivos
+    $comando->bind_param("ssss",$descricao,$data_entrega,$prioridade,$responsavel);
+    //5° executar comando na conexão de dados 
+    if ($comando->execute())
+    {
+        echo "<h1>tarefa marcada!</h1>";
+    }
+    else{
+        echo"<h1>Erro! Confira os dados! </h1>";
+    }
+
+    ?>
 </body>
 </html>
